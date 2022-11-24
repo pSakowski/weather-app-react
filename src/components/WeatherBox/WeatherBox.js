25c5bcd16dfe2c55f0a585b2953a31339a93938b
Piotr Sakowski <pees@MacBook-Air-Piotr.local>
2022-11-24 22:49:00 +0100

    add handleSubmit & handleCityChange

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,25 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleCityChange = useCallback(city => {
+    console.log(city)
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=b36ea99a96b6b7040b6500fd5e896f96&units=metric`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      
+    });
+  });
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange}/>
       <WeatherSummary />
       <Loader />
     </section>
